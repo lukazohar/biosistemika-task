5ba20861e83c898c1afe3071333ab19ec2f969c0
Luka Žohar <luka.zohi@gmail.com>
2023-12-24 00:55:53 +0100

    Created GeneratePlateContent function and needed class (for readability)

--- Biosistemika task/Program.cs
+++ Biosistemika task/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,6 +18,31 @@ namespace Biosistemika_task
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
+
+        static void GeneratePlateContent(
+            int plateSize,
+            List<List<string>> sampleNames,
+            List<List<string>> reagentNames,
+            List<int> numberOfReplicatesPerExperiment,
+            int maxAllowedPlates
+        )
+        {
+            
+        }
+    }
+
+    class Plate
+    {
+        public Plate() { this.Wells = new List<Well>(); }
+        public List<Well> Wells;
+    }
+
+    class Well
+    {
+        public Well() { }
+        public string SampleName;
+        public string ReagentName;
     }
 }
